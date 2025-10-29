315d32c5931c3cf5a603080684f4ed2d77c0c3da
Sergio Daniel Zacarias <sergiodcarp@gmail.com>
2025-10-29 02:17:59 -0300

    Fin ej 9

--- js/index.js
+++ js/index.js
@@ -5,20 +5,20 @@
 // división y módulo.
 // Verificar con isNaN() si los datos ingresados son válidos para evitar errores.
 
-// let primerNumero = parseFloat(prompt("Ingrese primer numero: "));
-// let segundoNumero = parseFloat(prompt("Ingrese segundo numero: "));
-// let verificacionNumeroUno = isNaN(primerNumero);
-// let verificacionDos = isNaN(segundoNumero);
+let primerNumero = parseFloat(prompt("Ingrese primer numero: "));
+let segundoNumero = parseFloat(prompt("Ingrese segundo numero: "));
+let verificacionNumeroUno = isNaN(primerNumero);
+let verificacionDos = isNaN(segundoNumero);
 
-// if (verificacionNumeroUno || verificacionDos == true) {
-//   alert("Solo se permiten numeros,vuelva a intentar resfrescando la pagina");
-// } else {
-//   console.log("La suma es: ", primerNumero + segundoNumero);
-//   console.log("La resta es: ", primerNumero - segundoNumero);
-//   console.log("La division es: ", primerNumero / segundoNumero);
-//   console.log("La multiplicacion es: ", primerNumero * segundoNumero);
-//   console.log("El modulo es es: ", primerNumero % segundoNumero);
-// }
+if (verificacionNumeroUno || verificacionDos == true) {
+  alert("Solo se permiten numeros,vuelva a intentar resfrescando la pagina");
+} else {
+  console.log("La suma es: ", primerNumero + segundoNumero);
+  console.log("La resta es: ", primerNumero - segundoNumero);
+  console.log("La division es: ", primerNumero / segundoNumero);
+  console.log("La multiplicacion es: ", primerNumero * segundoNumero);
+  console.log("El modulo es es: ", primerNumero % segundoNumero);
+}
 
 // Qué tenés que hacer:
 // Pedir nombre y edad con prompt().
