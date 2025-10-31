d9efffae00bce958fafd60a2bc4d181da1bd4f43
Sergio Daniel Zacarias <sergiodcarp@gmail.com>
2025-10-31 01:23:36 -0300

    Practica sobre ej 10, bucles y condicionales

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-// Ejercicio clase 9
+/*
+// Ejercicios clase 9
+// Numero 1
 // Pedir al usuario dos números usando prompt().
 // Convertir esos datos a números con parseFloat().
 // Mostrar en la consola los resultados de suma, resta, multiplicación,
@@ -20,6 +22,7 @@ if (verificacionNumeroUno || verificacionDos == true) {
   console.log("El modulo es es: ", primerNumero % segundoNumero);
 }
 
+// Numero 2
 // Qué tenés que hacer:
 // Pedir nombre y edad con prompt().
 // Validar que la edad sea un número válido con isNaN().
@@ -41,3 +44,48 @@ if (isNaN(edad)) {
 } else {
   console.log("Bienvenido ", nombre, "su edad es: ", edad, "años.");
 }
+
+
+
+// Ejercicio clase 10
+// Numero 1
+// Declarar tres variables: nombre, correo y mensaje, asignándoles valores de prueba.
+// Crear una función que valide que ninguna de estas variables esté vacía.
+// Si todos los campos tienen texto, mostrar por consola: "Formulario completo. Listo para enviar."
+// Si falta completar alguno, mostrar:"Faltan completar campos obligatorios."
+// Tips claves:
+//  Usá el operador && para verificar que todas las variables contengan texto.
+// Jugá cambiando los valores de prueba para ver cómo responde tu validación.
+// let nombre = prompt("Ingrese su nombre: ");
+// let correo = prompt("Ingrese su correo: ");
+// let mensaje = prompt("Ingrese su mensaje: ");
+
+// if (nombre != "" && correo != "" && mensaje != "") {
+//   console.log("Formulario completo. Listo para enviar.");
+// } else {
+//   console.log("Faltan completar campos obligatorios.");
+// }
+
+// Numero 2
+// Crear un array de al menos 5 productos (por ejemplo: "Remera", "Pantalón", "Gorra").
+// Recorrer el array con un bucle for o for...of.
+// En cada iteración, mostrar el producto con un alert().
+// Al finalizar, enviar por consola el mensaje: "Lista de productos mostrada correctamente."
+// Tips claves:
+// Definí el array como const para practicar buenas prácticas de variables constantes.
+// Podés sumar un contador para contar cuántos productos se muestran.
+
+const productos = [
+  "Remera",
+  "Short",
+  "Botines",
+  "Pelota",
+  "Guantes",
+  "Buzos",
+  "Musculosas",
+];
+for (let i = 0; i < productos.length; i++) {
+  alert(productos[i]);
+}
+console.log("Lista de productos mostrada correctamente.");
+*/
